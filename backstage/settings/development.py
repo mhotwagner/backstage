1b175312775aecfbb5547cc472150482485d3217
hotwag <mhotwagner@gmail.com>
2016-01-24 14:21:21 -0500

    Add dev configurations

---
+++ backstage/settings/development.py
@@ -0,0 +1,13 @@
+import os
+
+import dj_database_url
+
+from .base import *
+
+DEBUG = True
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+
+DATABASES = {
+    'default': dj_database_url(DATABASE_URL)
+}
