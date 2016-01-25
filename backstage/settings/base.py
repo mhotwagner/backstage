bbc6872ec84b7999e4494200a701fbe90b242996
hotwag <mhotwagner@gmail.com>
2016-01-24 20:24:11 -0500

    Add foti app
     - Add Foto model
     - Add Pillow library

--- backstage/settings/base.py
+++ backstage/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     'facade',
     'opere',
+    'foti',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ foti/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from opere import models as opere_models
+
+
+class Foto(opere_models.Opera):
+    location = models.CharField(max_length=64, blank=False)
+    date = models.DateField(blank=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Foti'
