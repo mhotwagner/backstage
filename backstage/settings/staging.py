c07233e31554b02573b7aec9d685958af46b3baf
hotwag <mhotwagner@gmail.com>
2016-01-24 20:44:05 -0500

    Add settings for staging environment

--- backstage/settings/staging.py
+++ backstage/settings/staging.py
@@ -1,2 +1,24 @@
+import os
+
+import dj_database_url
+
 from .base import *
 
+DEBUG = False
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+# Media handling
+MEDIA_ROOT = BASE_DIR.child("media")
+
+# Static file handling
+STATIC_ROOT = 'static_sources'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
