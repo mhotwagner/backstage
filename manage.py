fedf8ad19bc77a2095096b6b71ff6045e10c3557
hotwag <mhotwagner@gmail.com>
2016-02-29 08:50:35 -0500

    Update manage.py to hanlde Heroku's call to syncdb

--- manage.py
+++ manage.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backstage.settings")
 
     from django.core.management import execute_from_command_line
+    
+    # Handly syncdb calls for Heroku
+    if sys.argv[1] == 'syncdb': sys.argv[1] = 'migrate'
 
     execute_from_command_line(sys.argv)
